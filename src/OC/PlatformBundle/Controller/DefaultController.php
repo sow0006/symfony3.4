6a64b6645fe8a05226f873a95d2bc8a5ff976904
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-01 15:41:04 +0200

    la premiere route

--- src/OC/PlatformBundle/Controller/DefaultController.php
+++ src/OC/PlatformBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
