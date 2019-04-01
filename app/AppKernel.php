d747ea6afb9c53de4cf408ca802cd7c711ce0705
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-01 18:03:14 +0200

    OC

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            //nouvelle ligne generee
             new OC\PlatformBundle\OCPlatformBundle(),
         ];
 
