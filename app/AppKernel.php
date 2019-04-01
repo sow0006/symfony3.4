41fa61e1a76e2056c010d0c12438ae1a274fd7d1
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-01 13:35:11 +0200

    bundle OCPlatformBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            //nouvelle ligne generee
+            new OC\PlatformBundle\OCPlatformBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
