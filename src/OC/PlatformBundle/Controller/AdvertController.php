6d6839d0a80a809dc96884b32ffb652d2f30fcf3
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-02 09:09:45 +0200

    debut des routes avancées

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -15,4 +15,25 @@ class AdvertController extends Controller
     $content = $this->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
     return $content;
   }
+
+  // on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour
+  // correspondre au paramètre {id} de la route
+  public function viewAction($id)
+  {
+    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+    // Ici, on récupèrera depuis la base de données
+    // l'annonce correspondant à l'id $id.
+    // Puis on passera l'annonce à la vue pour
+    // qu'elle puisse l'afficher
+
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+
+  public function viewSlugAction($slug, $year, $format)
+  {
+    return new Response("On pourrait afficher l'annonce correspondant du 
+    slug '".$slug."', créée en ".$year." et au format ".$format.".");
+  }
 }
