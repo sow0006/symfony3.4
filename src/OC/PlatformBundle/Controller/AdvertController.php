60d584f3dad538bf475867550f60527d9dd10bb0
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-02 10:12:34 +0200

    les parametres hors routes

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,6 +6,7 @@ namespace OC\PlatformBundle\Controller;
 
 // N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
@@ -13,7 +14,7 @@ class AdvertController extends Controller
   public function indexAction()
   {
        // Recuperer URL de l'annonce
-       $url = $this->generate('oc_platform_view', // 1er argument
+       $url = $this->generateURL('oc_platform_view', // 1er argument
                               array('id' => 5)    // 2eme argument
                              );
     return new Response ("L'URL de l'annonce d'id 5 est : ".$url);
@@ -22,7 +23,7 @@ class AdvertController extends Controller
   // on doit donc définir la méthode viewAction.
   // On donne à cette méthode l'argument $id, pour
   // correspondre au paramètre {id} de la route
-  public function viewAction($id)
+  public function viewAction($id, Request $request)
   {
     // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
 
@@ -30,8 +31,9 @@ class AdvertController extends Controller
     // l'annonce correspondant à l'id $id.
     // Puis on passera l'annonce à la vue pour
     // qu'elle puisse l'afficher
+    $tag = $request->query->get('tag');
 
-    return new Response("Affichage de l'annonce d'id : ".$id);
+    return new Response("Affichage de l'annonce d'id : ".$id.", avec le tag: ".$tag);
   }
 
   public function viewSlugAction($slug, $year, $_format)
