f70175bfe66e0122e8c008106c2e10437df303b3
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-02 16:33:26 +0200

    enregistrer ses entites en bdd

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -50,18 +50,15 @@ class AdvertController extends Controller
       'listAdverts' => $listAdverts));
   }
 
-  public function viewAction()
+  public function viewAction($id)
   {
-    /*$advert = array(
+    $advert = array(
       'title'   => 'Recherche développpeur Symfony2',
       'id'      => $id,
       'author'  => 'Alexandre',
       'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
       'date'    => new \Datetime()
-    );*/
-
-    $advert = new Advert;
-    $advert->setContent("Recherche développeur Symfony2.");
+    );
 	
     return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
       'advert' => $advert
@@ -70,16 +67,29 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-    // On récupère le service
-    $antispam = $this->container->get('oc_platform.antispam');
+    // Création de l'entité
+    $advert = new Advert();
+    $advert->setTitle('Recherche développeur Symfony2.');
+    $advert->setAuthor('Alexandre');
+    $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
+    // On peut ne pas définir ni la date ni la publication,
+    // car ces attributs sont définis automatiquement dans le constructeur
 
-    // Je pars du principe que $text contient le texte d'un message quelconque
-    $text = '';
-    if ($antispam->isSpam($text)) {
-      throw new \Exception('Votre message a été détecté comme spam !');
+    // On récupère l'EntityManager
+    $em = $this->getDoctrine()->getManager();
+
+    // Étape 1 : On « persiste » l'entité
+    $em->persist($advert);
+
+    // Étape 2 : On « flush » tout ce qui a été persisté avant
+    $em->flush();
+
+    // Reste de la méthode qu'on avait déjà écrit
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+      return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
     }
-    
-    // Si on n'est pas en POST, alors on affiche le formulaire
+
     return $this->render('OCPlatformBundle:Advert:add.html.twig');
   }
 
