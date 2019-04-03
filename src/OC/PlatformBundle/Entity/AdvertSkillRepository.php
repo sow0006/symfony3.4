f4446f8cd7099748472741f5dad64b9f9b829f8c
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-03 16:07:56 +0200

    gestion des valeurs par defauts du formulaire

---
+++ src/OC/PlatformBundle/Entity/AdvertSkillRepository.php
@@ -0,0 +1,11 @@
+<?php
+// src/OC/PlatformBundle/Entity/AdvertRepository.php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class AdvertSkillRepository extends EntityRepository
+{
+}
