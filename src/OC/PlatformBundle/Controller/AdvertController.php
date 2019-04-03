aca42da2fbea0c4ce5ec6476a86cd18ac556b459
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-03 09:15:14 +0200

    test de la relation Many-To-One OK

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,6 +6,7 @@ namespace OC\PlatformBundle\Controller;
 
 use OC\PlatformBundle\Entity\Advert;
 use OC\PlatformBundle\Entity\Image;
+use OC\PlatformBundle\Entity\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -53,24 +54,27 @@ class AdvertController extends Controller
 
   public function viewAction($id)
   {
-    // On récupère le repository
-    $repository = $this->getDoctrine()
-      ->getManager()
+    $em = $this->getDoctrine()->getManager();
+
+    // On récupère l'annonce $id
+    $advert = $em
       ->getRepository('OCPlatformBundle:Advert')
+      ->find($id)
     ;
 
-    // On récupère l'entité correspondante à l'id $id
-    $advert = $repository->find($id);
-
-    // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
-    // ou null si l'id $id  n'existe pas, d'où ce if :
     if (null === $advert) {
       throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
     }
 
-    // Le render ne change pas, on passait avant un tableau, maintenant un objet
+    // On récupère la liste des candidatures de cette annonce
+    $listApplications = $em
+      ->getRepository('OCPlatformBundle:Application')
+      ->findBy(array('advert' => $advert))
+    ;
+
     return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
-      'advert' => $advert
+      'advert'           => $advert,
+      'listApplications' => $listApplications
     ));
   }
 
@@ -78,19 +82,25 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-    // Création de l'entité
+    // Création de l'entité Advert
     $advert = new Advert();
     $advert->setTitle('Recherche développeur Symfony2.');
     $advert->setAuthor('Alexandre');
     $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
 
-    // Création de l'entité Image
-    $image = new Image();
-    $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
-    $image->setAlt('Job de rêve');
+    // Création d'une première candidature
+    $application1 = new Application();
+    $application1->setAuthor('Marine');
+    $application1->setContent("J'ai toutes les qualités requises.");
 
-    // On lie l'image à l'annonce
-    $advert->setImage($image);
+    // Création d'une deuxième candidature par exemple
+    $application2 = new Application();
+    $application2->setAuthor('Pierre');
+    $application2->setContent("Je suis très motivé.");
+
+    // On lie les candidatures à l'annonce
+    $application1->setAdvert($advert);
+    $application2->setAdvert($advert);
 
     // On récupère l'EntityManager
     $em = $this->getDoctrine()->getManager();
@@ -98,12 +108,14 @@ class AdvertController extends Controller
     // Étape 1 : On « persiste » l'entité
     $em->persist($advert);
 
-    // Étape 1 bis : si on n'avait pas défini le cascade={"persist"},
-    // on devrait persister à la main l'entité $image
-    // $em->persist($image);
+    // Étape 1 bis : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
+    // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
+    $em->persist($application1);
+    $em->persist($application2);
 
-    // Étape 2 : On déclenche l'enregistrement
+    // Étape 2 : On « flush » tout ce qui a été persisté avant
     $em->flush();
+
     
     // Reste de la méthode qu'on avait déjà écrit
     if ($request->isMethod('POST')) {
