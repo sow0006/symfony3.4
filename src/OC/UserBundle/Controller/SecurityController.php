ac3198839382d5517352e0f5dd37008789a4fc05
Stagiaire2 <stagiaire2@asgadev.local>
2019-04-05 10:49:15 +0200

    configuration de la securite : check

--- src/OC/UserBundle/Controller/SecurityController.php
+++ src/OC/UserBundle/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace OC\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller
 {
@@ -14,7 +15,7 @@ class SecurityController extends Controller
     if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
       return $this->redirectToRoute('oc_platform_accueil');
     }
-
+    
     // Le service authentication_utils permet de récupérer le nom d'utilisateur
     // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
     // (mauvais mot de passe par exemple)
